699463fe3eb6b8d4bc967bf5e498b802f9661990
Isaac Waller <isaacwaller@Isaacs-MacBook-Pro.local>
2024-08-14 16:51:41 +1000

    Implement requests

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,9 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Counter: a
     .model({
-      content: a.string(),
+      value: a.integer(),
     })
     .authorization((allow) => [allow.guest()]),
 });
@@ -18,9 +18,6 @@ export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'iam',
-  },
 });
 
 /*== STEP 2 ===============================================================
@@ -28,7 +25,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+import { generateClient } from "aws-amplify/data";
+
+Amplify.configure({
+  ...outputs,
+  Auth: {
+    Cognito: { allowGuestAccess: true },
+  },
+});
+
+const client = generateClient();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counters, setCounters] = useState([])
+
+  useEffect(() => {
+    async function getCounters() {
+      const response = await client.models.Counter.list({ authMode: "identityPool" });
+      setCounters(response.data);
+    }
+
+    getCounters();
+  }, []);
+
+  async function createCounter() {
+    const response = await client.models.Counter.create(
+      { value: 0 },
+      { authMode: "identityPool" },
+    );
+
+    setCounters((oldCounters) => [...oldCounters, {
+      id: response.data.id,
+      value: 0,
+    }]);
+  }
+
+  function updateCounter(id, newValue) {
+    client.models.Counter.update(
+      {
+        id,
+        value: newValue,
+      },
+      { authMode: "identityPool" },
+    );
+
+    setCounters((oldCounters) => {
+      const newCounters = [...oldCounters];
+
+      const counterToUpdate = newCounters.find((counter) => {
+        return counter.id === id;
+      });
+
+      counterToUpdate.value++;
+      return newCounters;
+    });
+  }
+
+  function deleteCounter(id) {
+    client.models.Counter.delete(
+      { id },
+      { authMode: "identityPool" },
+    );
+
+    setCounters((oldCounters) => {
+      const newCounters = oldCounters.filter((counter) => {
+        return counter.id !== id;
+      });
+
+      return newCounters;
+    });
+  }
+
+  const countersContent = counters.map((counter) => {
+    return <div key={counter.id}>
+      <button onClick={() => updateCounter(counter.id, counter.value + 1)}>
+        count is {counter.value}
+      </button>
+
+      <button onClick={() => deleteCounter(counter.id)}>-</button>
+    </div>;
+  });
 
   return (
     <>
@@ -17,10 +96,10 @@ function App() {
         </a>
       </div>
       <h1>Amplify Application</h1>
+      <div className="card">{countersContent}</div>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={createCounter}>+</button>
       </div>
     </>
   )
